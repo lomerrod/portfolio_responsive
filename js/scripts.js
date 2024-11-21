e1e2effc914edec6953b43dcb94cd0e01f20059d
lomerrod <lomerrod@gmail.com>
2024-11-21 14:23:13 -0300

    Delete js directory

--- js/scripts.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Gracias por tu mensaje. Me pondré en contacto pronto.');
-    this.reset(); // Limpiar formulario después de enviar
-});
